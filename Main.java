d1a96fdd8c4ba337c37168e4c121ed6fa9b1f21a
StopaKipling2019 <42677944+StopaKipling2019@users.noreply.github.com>
2018-10-07 16:32:00 -0700

    Code for base damage change, intelligence 10

--- Main.java
+++ Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        //choose intelligence, keep base damage the same
-        int intelligence = 21;
+        //choose base damage, keep intelligence the same
+        int intelligence = 10;
         Random rand = new Random();
-        int baseDmg = 5;
+        int baseDmg = 11;
         double damageInt = intelligence*baseDmg;
 
-            //test 10 times and add up total damage
+        //test 10 times and add up total damage
 
         double dmgMod1 = rand.nextDouble() + 0.5;
         double damage1 = damageInt * dmgMod1;
@@ -46,9 +46,10 @@ public class Main {
         double damageTot = damage1 + damage2 + damage3 + damage4 + damage5 + damage6 + damage7 + damage8 + damage9 +damage10;
         System.out.println(damageTot);
 
-        //20 was 50 too 60 percent of time 21 was 60 to 70 percent of time
+        //10 50-60 percent 11 was 60-70 percent
 
-        System.out.println(intelligence);
+        System.out.println(baseDmg);
+
 
 
 
